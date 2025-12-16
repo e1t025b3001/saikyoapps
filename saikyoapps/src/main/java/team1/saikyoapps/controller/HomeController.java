62ccdc13e1ffe32dfff2efa126139037793089b0
e1b23030 <e1b23030@oit.ac.jp>
2025-12-16 15:52:58 +0900

    C:/oit/isdev25-byod/PortableGit-2.50.0-64/i18nに対するポストに対応

--- saikyoapps/src/main/java/team1/saikyoapps/controller/HomeController.java
+++ saikyoapps/src/main/java/team1/saikyoapps/controller/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -18,8 +19,21 @@ public class HomeController {
     } else {
       model.addAttribute("username", "guest");
     }
-    
+
     return "index";
+  }
+
+  @PostMapping("/i18n")
+  public String setI18n(@RequestParam String i18n, Model model, Authentication authentication) {
+    model.addAttribute("i18n", i18n);
+    System.err.println("i18n set to: " + i18n);
+    if (authentication != null) {
+      model.addAttribute("username", authentication.getName());
+    } else {
+      model.addAttribute("username", "guest");
+    }
+    return "index";
+
   }
 
   @GetMapping("/login")
